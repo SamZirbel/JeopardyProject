ade64ed44e7041211b8fff08cd97cbecab020d3e
Sam Zirbel <samuelzirbel@gmail.com>
2021-10-28 11:24:43 -0700

    Updated Model And Service

--- JeopardyProject/src/app/models/question.ts
+++ JeopardyProject/src/app/models/question.ts
@@ -1,2 +1,5 @@
 export class Question {
+
+  constructor(public question:string, public answer:string, value:number);
+
 }
--- JeopardyProject/src/app/services/question.service.ts
+++ JeopardyProject/src/app/services/question.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Question } from '../models/question';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getQuestion():Observable<Question> {
+
+    return this.http.get("https://jservice.io/api/random") as Observable<Question>
+
+  }
+
 }
